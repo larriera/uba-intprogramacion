60a2bde8a3b535283cc54594aa70a85c6c2668f2
Pilar L <110357749+plarriera@users.noreply.github.com>
2024-09-25 20:54:23 -0300

    añadir casos de test ej 1

--- modelos_1p/siststockTest.hs
+++ modelos_1p/siststockTest.hs
@@ -1,2 +1,12 @@
 import SistStock
 import Test.HUnit
+
+-- EJERCICIO 1
+testGenerarStock = test [
+    "lista vacia" ~: (generarStock []) ~?= [],
+    "1 solo elemento" ~: (generarStock ["a"]) ~?= [("a",1)],
+    "todos elementos repetidos" ~: (generarStock ["a","a","a"]) ~?= [("a",1)],
+    "todos elementos distintos" ~: (generarStock ["a","b","c"]) ~?= [("a",1),("b",1),("c",1)],
+    "varios con repeticiones ordenadas" ~: (generarStock ["a","b","b","c"]) ~?= [("a",1),("b",2),("c",1)],
+    "varios con repeticiones mezcladas" ~: (generarStock ["a","b","a","a","c","a","d","c"]) ~?= [("a",4),("b",1),("c",2),("d",1)],
+]
